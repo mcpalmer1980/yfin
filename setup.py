a9ed8c47582502bf6f52019b688860b66c838764
mcpalmer1980 <michaelcpalmer1980@gmail.com>
2019-12-30 17:02:31 -0500

    added ticks command to setup.py
    added ticks command to setup.py
    added requirements to setup.py
      ticks.py
      classes.py
      PyInquirer

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     author='Chris Palmieri',
     description='Scrape yahoo financial data using yahoo-fin from the command line',
     version='0.1',
-    py_modules=['yfin', 'stock_info'],
+    py_modules=['yfin', 'ticks', 'stock_info', 'classes'],
     install_requires=[
-        'Click', 'pandas', 'requests_html', 'Blessings',
+        'Click', 'pandas', 'requests_html', 'Blessings', 'PyInquirer',
     ],
     entry_points='''
         [console_scripts]
         yfin=yfin:main
+        ticks=ticks:main
     ''',
 )
